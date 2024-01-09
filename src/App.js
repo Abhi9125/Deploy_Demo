d696769fd63e5c382d083600a9c51592ff85398a
Abhi9125 <abhi.singh3132@gmail.com>
2024-01-09 23:13:31 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
+        <p>Hello World Learning how to deply project to netplify.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
